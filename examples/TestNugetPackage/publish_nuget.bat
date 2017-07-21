ab56218cf2f37a141fd372b041e1478d21af00d9
Ian Escarro <ian.escarro@gmail.com>
2017-07-21 09:15:05 +0800

    changed the file generated and added a comment about the file might not be the same file as in this batch file.

--- examples/TestNugetPackage/publish_nuget.bat
+++ examples/TestNugetPackage/publish_nuget.bat
@@ -1,3 +1,5 @@
 nuget pack TestNugetPackage.csproj 
 nuget setApiKey dd59faab-b254-4c68-b80e-d753e7c56e24 -Source http://localhost:16714/nuget
-nuget push TestNugetPackage.1.0.6410.36796.nupkg -Source http://localhost:16714/nuget
+
+rem Please check the generated .nupkg file below. This should reflect what is generated from packaging the project above.
+nuget push TestNugetPackage.1.0.6411.16520.nupkg -Source http://localhost:16714/nuget
